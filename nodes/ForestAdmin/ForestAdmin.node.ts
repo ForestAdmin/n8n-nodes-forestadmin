4fbe9d8d1058b10022506f2f7137503e57d20260
Sandro Munda <sandro@munda.me>
2023-06-15 11:29:56 +0200

    Bump 0.1.11

--- nodes/ForestAdmin/ForestAdmin.node.ts
+++ nodes/ForestAdmin/ForestAdmin.node.ts
@@ -1,4 +1,6 @@
 import {
+	IDataObject,
+	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
 	IWebhookFunctions,
@@ -23,18 +25,130 @@ export class ForestAdmin implements INodeType {
 		webhooks: [{
 			name: 'default',
 			httpMethod: 'POST',
-			responseMode: 'lastNode',
 			path: 'webhook',
+			responseCode: '={{$parameter["responseCode"]}}',
+			responseMode: '={{$parameter["responseMode"]}}',
+			responseData:
+				'={{$parameter["responseData"] || ($parameter.options.noResponseBody ? "noData" : undefined) }}',
+			responseBinaryPropertyName: '={{$parameter["responseBinaryPropertyName"]}}',
+			responseContentType: '={{$parameter["options"]["responseContentType"]}}',
+			responsePropertyName: '={{$parameter["options"]["responsePropertyName"]}}',
+			responseHeaders: '={{$parameter["options"]["responseHeaders"]}}',
 		}],
-		properties: [],
+		properties: [
+			{
+				displayName: 'Respond',
+				name: 'responseMode',
+				type: 'options',
+				options: [
+					{
+						name: 'Immediately',
+						value: 'onReceived',
+						description: 'As soon as this node executes',
+					},
+					{
+						name: 'When Last Node Finishes',
+						value: 'lastNode',
+						description: 'Returns data of the last-executed node',
+					},
+					{
+						name: "Using 'Respond to Webhook' Node",
+						value: 'responseNode',
+						description: 'Response defined in that node',
+					},
+				],
+				default: 'onReceived',
+				description: 'When and how to respond to the webhook',
+			},
+			{
+				displayName:
+					'Insert a \'Respond to Webhook\' node to control when and how you respond. <a href="https://docs.n8n.io/integrations/builtin/core-nodes/n8n-nodes-base.respondtowebhook/" target="_blank">More details</a>',
+				name: 'webhookNotice',
+				type: 'notice',
+				displayOptions: {
+					show: {
+						responseMode: ['responseNode'],
+					},
+				},
+				default: '',
+			},
+			{
+				displayName: 'Response Code',
+				name: 'responseCode',
+				type: 'number',
+				displayOptions: {
+					hide: {
+						responseMode: ['responseNode'],
+					},
+				},
+				typeOptions: {
+					minValue: 100,
+					maxValue: 599,
+				},
+				default: 200,
+				description: 'The HTTP Response code to return',
+			},
+			{
+				displayName: 'Response Data',
+				name: 'responseData',
+				type: 'options',
+				displayOptions: {
+					show: {
+						responseMode: ['lastNode'],
+					},
+				},
+				options: [
+					{
+						name: 'All Entries',
+						value: 'allEntries',
+						description: 'Returns all the entries of the last node. Always returns an array.',
+					},
+					{
+						name: 'First Entry JSON',
+						value: 'firstEntryJson',
+						description:
+							'Returns the JSON data of the first entry of the last node. Always returns a JSON object.',
+					},
+					{
+						name: 'First Entry Binary',
+						value: 'firstEntryBinary',
+						description:
+							'Returns the binary data of the first entry of the last node. Always returns a binary file.',
+					},
+					{
+						name: 'No Response Body',
+						value: 'noData',
+						description: 'Returns without a body',
+					},
+				],
+				default: 'firstEntryJson',
+				description:
+					'What data should be returned. If it should return all items as an array or only the first item as object.',
+			},
+		],
 	};
 
 	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
-		// const req = this.getRequestObject();
-		
+		const options = this.getNodeParameter('options', {}) as IDataObject;
+		const headers = this.getHeaderData();
+
+		const response: INodeExecutionData = {
+			json: {
+				headers,
+				params: this.getParamsData(),
+				query: this.getQueryData(),
+				body: this.getBodyData(),
+			},
+		};
+
+		let webhookResponse: string | undefined;
+		if (options.responseData) {
+			webhookResponse = options.responseData as string;
+		}
+
 		return {
-			workflowData: [
-			],
+			webhookResponse,
+			workflowData: [[response]],
 		};
 	}
 }
